f2e2283b62cb96e842d5390c4f7f1dfaf484b21e
João Pedro <jp.bahia.zica@gmail.com>
2020-09-06 16:40:48 -0300

    Adding typescript and rollup-typescritp-plugin2

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from '@rollup/plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import pkg from "./package.json"
 import { terser } from 'rollup-plugin-terser'
