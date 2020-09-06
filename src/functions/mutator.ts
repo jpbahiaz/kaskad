14e5fc6095e097d243f3b49d6440c96a827685cb
João Pedro <jp.bahia.zica@gmail.com>
2020-09-06 20:21:11 -0300

    Typing mutator and createElement

---
+++ src/functions/mutator.ts
@@ -0,0 +1,16 @@
+export function mutator(
+	entity: any,
+	callback: (entt: any, prevEntt: any) => void = function(){}
+) {
+	return function mutate(prop: string, value: unknown) {
+		let prevEntity = {}
+		if(entity instanceof Node){
+			prevEntity = entity.cloneNode(true)
+		} else {
+			prevEntity = { ...entity }
+		}
+		entity[prop] = value
+		callback(entity, prevEntity)
+		return entity
+	}
+}
