8834e4a2409c7bce1bce76fcd010043d7ceb455c
João Pedro <jp.bahia.zica@gmail.com>
2020-09-17 21:34:00 -0300

    Pass state to store subscribe

--- src/redux/registerStore.ts
+++ src/redux/registerStore.ts
@@ -1,3 +1,13 @@
-export function registerStore(store: any) {
-	return { subscribe: store.subscribe, dispatch: store.dispatch }
+function subscriber<TState = any>(store: any) {
+	return function subscribe(callback: (state: TState) => void, path?: string[]) {
+		const unsubscribe = store.subscribe(() => {
+			callback(store.getState())
+		})
+
+		return unsubscribe
+	}
+}
+
+export function registerStore<TState = any>(store: any) {
+	return { subscribe: subscriber<TState>(store), dispatch: store.dispatch }
 }
