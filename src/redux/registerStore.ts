f8cb6ce136384f35105a2f9cfef38d0e19d09e25
João Pedro <jp.bahia.zica@gmail.com>
2020-09-18 15:04:57 -0300

    Adding path functionality to redux subscribe

--- src/redux/registerStore.ts
+++ src/redux/registerStore.ts
@@ -1,13 +1,28 @@
-function subscriber<TState = any>(store: any) {
-	return function subscribe(callback: (state: TState) => void, path?: string[]) {
-		const unsubscribe = store.subscribe(() => {
-			callback(store.getState())
-		})
+function path (obj: any, slicePath: string[]) {
+	return slicePath.reduce((previous: any, current: string) => previous[current], obj)
+}
 
+function subscriber(store: any) {
+	return function subscribe<TSlice = any>(callback: (slice: TSlice) => void, slicePath?: string[]) {
+		let unsubscribe
+		if (slicePath) {
+			let prevPathState = path(store.getState(), slicePath)
+			unsubscribe = store.subscribe(() => {
+				const currentPathState = path(store.getState(), slicePath)
+				if (!Object.is(prevPathState, currentPathState)) {
+					callback(currentPathState)
+					prevPathState = currentPathState
+				}
+			})
+		} else {
+			unsubscribe = store.subscribe(() => {
+				callback(store.getState())
+			})
+		}
 		return unsubscribe
 	}
 }
 
-export function registerStore<TState = any>(store: any) {
-	return { subscribe: subscriber<TState>(store), dispatch: store.dispatch }
+export function registerStore(store: any) {
+	return { subscribe: subscriber(store), dispatch: store.dispatch }
 }
