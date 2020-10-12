929747833b79e20b7cdbcef7b45cec83c3fd999d
João Pedro <jp.bahia.zica@gmail.com>
2020-10-12 14:27:20 -0300

    Inital Root and Component prototypes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
+function Component(parent, name) {
+	this.parent = parent
+	this.ref = { galba: 'veloso' }
+	this.next = () => {}
+	this.name = name
+
+	Object.freeze(this)
+}
+Component.prototype.use = function (...args) {
+	console.log('component.use', this.name, args)
+	args.forEach(fn => {
+		typeof fn === 'function' && fn({}, this, this.next)
+	})
+}
+Component.prototype.append = function (...args) {
+	console.log('component.append', this.name, args)
+	args.forEach(fn => {
+		const newComponent = new Component(this.ref)
+		typeof fn === 'function' && fn({}, newComponent, newComponent.next)
+	})
+}
+
+function Root() {
+	Component.call(this, null, 'root')
+}
+Root.prototype = Object.create(Component.prototype)
+
 function kaskad() {
-	let useCall = 0
-	
-	function next() {}
-	
-	function use (...args) {
-		console.log('use call', useCall++, args)
-		args.forEach(fn => {
-			typeof fn === 'function' && fn({}, { use }, next)
-			// typeof fn === 'string' && console.log(fn)
-		})
-	}
-	
-	return {
-		use
-	}
+	return new Root()
 }
 
 export default kaskad
