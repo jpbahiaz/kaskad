33bdd34e6a73b38c1bc48f000bf2bffe61fe4e7b
Zica <jp.bahia.zica@gmail.com>
2020-10-25 18:53:07 -0300

    Restarting

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-import Root from "./core/root"
 function kaskad() {
-	return Root().instance
 }
 
-export {
-	Middleware,
-	ComponentInstance,
-	RootInstance,
-	TRoot,
-} from './core/types'
 export default kaskad
--- src/tree/vnode.ts
+++
@@ -1,7 +0,0 @@
-import { TVnode } from './types'
-
-function Vnode(parent: TVnode, child: TVnode, sibling: TVnode, tag: any, domRef: any): TVnode {
-	return { parent: parent, child: child, sibling: sibling, tag: tag, domRef: domRef, render: null }
-}
-
-export default Vnode
