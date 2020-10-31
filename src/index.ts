24b4d909d7c35aabfe6706244334bc88cbd5499d
Zica <jp.bahia.zica@gmail.com>
2020-10-30 21:53:58 -0300

    Stop reinventing the wheel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
-function kaskad() {
+const EMPTY_ARR = []
+const isArray = Array.isArray
+
+function createVnode(type, props, children, node, key, tag?) {
+	return {
+		type,
+		props,
+		children,
+		node,
+		key,
+		tag
+	}
 }
 
-export default kaskad
+
+export function k(type, props?, children?) {
+	return createVnode(
+		type,
+		props || null,
+		isArray(children) ? children : children == null ? EMPTY_ARR : [children],
+		null,
+		props ? props.key || null : null,
+	)	
+}
+
+export function app (options) {
+	return options
+}
